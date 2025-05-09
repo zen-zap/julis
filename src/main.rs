7ba9a3470ea15050a8ff1bf06bbe54c268bdb2ac
Ashutosh-Panda-340 <pandaashutosh340@gmail.com>
2025-05-09 18:08:50 +0530

    something?

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ use winit::{
 	window::{WindowBuilder},
 };
 
+mod mandel;
+
 fn main() {
     let event_loop = EventLoop::new().unwrap();
 
@@ -43,9 +45,40 @@ fn main() {
             } => {
 
                 let frame = pixels.frame_mut();
+                let width = size.width;
+                let height = size.height;
 
-                for pixel in frame.chunks_exact_mut(4) {
-                    pixel.copy_from_slice(&[0,0,0,255]); // each pixel set to black with full opacity
+                let zoom = 1.0;
+                let offset_x = -0.5;
+                let offset_y = 0.0;
+                let max_iter = 100;
+
+                for y in 0..height {
+                    for x in 0..width {
+                        let (c_re, c_im) = mandel::pixel_to_complex(
+                            x,
+                            y,
+                            width,
+                            height,
+                            zoom,
+                            offset_x,
+                            offset_y,
+                        );
+                        let iter = mandel::mandelbrot(c_re, c_im, max_iter);
+
+                        let pixel_index = ((y * width + x) * 4) as usize;
+                        let color = if iter == max_iter {
+                            [0, 0, 0, 255] // Black for points inside the set
+                        } else {
+                            let t = iter as f64 / max_iter as f64;
+                            let r = (9.0 * (1.0 - t) * t * t * t * 255.0) as u8;
+                            let g = (15.0 * (1.0 - t) * (1.0 - t) * t * t * 255.0) as u8;
+                            let b = (8.5 * (1.0 - t) * (1.0 - t) * (1.0 - t) * t * 255.0) as u8;
+                            [r, g, b, 255]
+                        };
+
+                        frame[pixel_index..pixel_index + 4].copy_from_slice(&color);
+                    }
                 }
 
                 pixels.render().unwrap();
